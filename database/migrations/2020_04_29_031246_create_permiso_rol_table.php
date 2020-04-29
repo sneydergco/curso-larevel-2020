0e92ebb48ce183e47602d55363c62878b131fcd6
sneydergco <sneydergco@gmail.com>
2020-04-28 22:54:48 -0500

    commit migration 2

---
+++ database/migrations/2020_04_29_031246_create_permiso_rol_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePermisoRolTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('permiso_rol', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('rol_id');
+            $table->foreign('rol_id', 'fk_permisorol_rol')->references('id')->on('rol')->onDelete('restrict')->onUpdate('restrict');
+            $table->unsignedInteger('permiso_id');
+            $table->foreign('permiso_id', 'fk_permisorol_permiso')->references('id')->on('permiso')->onDelete('restrict')->onUpdate('restrict');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('permiso_rol');
+    }
+}
